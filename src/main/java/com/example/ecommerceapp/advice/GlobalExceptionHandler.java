4c8daeeb4fbfa6aebdc9914f10ead6955b46d21b
Girlescu Eugen <eugeng@operative.com>
2023-10-16 20:18:15 +0300

    Added spring security

--- src/main/java/com/example/ecommerceapp/advice/GlobalExceptionHandler.java
+++ src/main/java/com/example/ecommerceapp/advice/GlobalExceptionHandler.java
@@ -21,28 +21,25 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
     }
 
-    @ControllerAdvice
-    public class CustomErrorHandler {
 
-        @ResponseStatus(HttpStatus.BAD_REQUEST)
-        @ExceptionHandler(MethodArgumentNotValidException.class)
-        @ResponseBody
-        public ResponseEntity<Map<String, Object>> handleValidationExceptions(
-                MethodArgumentNotValidException ex) {
-            Map<String, String> errors = new HashMap<>();
-            ex.getBindingResult().getAllErrors().forEach((error) -> {
-                String fieldName = ((FieldError) error).getField();
-                String errorMessage = error.getDefaultMessage();
-                errors.put(fieldName, errorMessage);
-            });
-            Map<String, Object> response = new HashMap<>();
-            response.put("timestamp", new Date());
-            response.put("status", HttpStatus.BAD_REQUEST.value());
-            response.put("error", "Bad Request");
-            response.put("message", "Validation failed");
-            response.put("errors", errors);
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    public ResponseEntity<Map<String, Object>> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        Map<String, Object> response = new HashMap<>();
+        response.put("timestamp", new Date());
+        response.put("status", HttpStatus.BAD_REQUEST.value());
+        response.put("error", "Bad Request");
+        response.put("message", "Validation failed");
+        response.put("errors", errors);
 
-            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-        }
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
